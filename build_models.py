8fc3b52d50c63f0c474d6548a2863d364c5d17ab
TalentuMagic <davidioan2001@gmail.com>
2024-02-07 23:38:40 +0200

    added argument parser

--- build_models.py
+++ build_models.py
@@ -1,6 +1,19 @@
 import network_regression
 import network_classification
 import checker
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Build both Regression and Classification models for the parsed pie.")
+
+parser.add_argument('Pie', metavar='pie', type=int,
+                    help='Please select one of the available pies.')
+parser.add_argument('--PriceHistory', metavar='price_history', type=str, default='n',
+                    help="Choose 'y' if you want to see price history.")
+parser.add_argument('--ModelPerformance', metavar='model_performance', type=str,
+                    default='n', help="Choose 'y' if you want to see model(s) performance.")
+
+args = parser.parse_args()
 
 
 def build_models_regression(user_choice: int = None, price_history: str = None, metrics: str = None):
@@ -16,9 +29,10 @@ def compare_models(user_choice: int = None, price_history: str = None, metrics: 
 
 
 def main():
-    build_models_classification(1, 'n', 'n')
-    build_models_regression(1, 'n', 'n')
-    compare_models(1, 'n', 'n')
+    build_models_classification(
+        args.Pie, args.PriceHistory, args.ModelPerformance)
+    build_models_regression(args.Pie, args.PriceHistory, args.ModelPerformance)
+    compare_models(args.Pie, args.PriceHistory, args.ModelPerformance)
 
 
 if __name__ == "__main__":
